85a0b9fc6264f58d97978a534455cc995f4498a4
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-12 20:05:04 +0600

    readme updated

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const Nav = () => {
   return (
     <nav className="flex justify-between  my-10">
-      <div>Quiz Master</div>
-      <div className="flex justify-end gap-10">
+      <div className="text-2xl font-bold text-indigo-700">Quiz Master</div>
+      <div className="flex justify-end gap-10 text-bold text-xl text-indigo-500">
         <Link to={"/topics"}>Topics</Link>
         <Link to={"/statistics"}>Statistics</Link>
         <Link to={"/blog"}>Blog</Link>
--- src/components/Option.js
+++ src/components/Option.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
 const Option = ({ option, correctAnswer }) => {
   const checkAnswer = () => {
     if (correctAnswer === option) {
       toast.success("Correct answer");
     } else {
-      toast.error("Invalid answer");
+      toast.error("Wrong answer... try again");
     }
   };
   return (
@@ -14,8 +15,6 @@ const Option = ({ option, correctAnswer }) => {
       <ToastContainer />
       <input
         type="checkbox"
-        id="vehicle1"
-        name="vehicle1"
         value={option}
         className="my-5 mx-2"
         onChange={checkAnswer}
